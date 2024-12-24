1cc2dc6091aa7fb5058f791d031d368120bd163b
Jayachandra_Kumar <97940002+Jayachandrakumar21@users.noreply.github.com>
2024-12-24 20:47:24 +0530

    Update Linux-cmd.sh

--- Linux-cmd.sh
+++ Linux-cmd.sh
@@ -9,9 +9,24 @@ cd -> change directory
 mv -> move files/directory
 rm -> remove/Delete files/directories
 echo -> print
+cat -> to display file content on the terminal
 vim -> vi improved file Editor
 sed 
 chmod -> changing file permission
 vi -> file editor
 su -> switch user
-grep
+grep -> row filter
+awk -> column filter
+useradd
+passwd 
+userdel
+chmod
+chown
+top
+free
+netstat
+ps
+ifconfig
+id
+
+
