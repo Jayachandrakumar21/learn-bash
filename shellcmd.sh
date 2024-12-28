18bc95826d0930c639154486753b433a873d2efc
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-28 16:12:56 +0530

    ls cmd

--- shellcmd.sh
+++ shellcmd.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-uname -a
-lsblk
+#created by Jayachandra_kumar 28-12-24
+#linux commands
+ls  #list content
+ls -l #long list with details content
+ls -a #list all files and hidden files
+ls -A #list all files and hidden files
+ls -lh
+ls -lt
+ls -ltr
+ls -ltrh
+#uname -a
+#lsblk
 #cat /etc/os-release
 #cat /proc/meminfo
 #cat /proc/cpuinfo
