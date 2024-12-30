c907ea55b1141058751c0249f426d9e2a6df4a4b
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 15:35:39 +0530

    updated conversation

---
+++ variables5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:30-12-2024
+#Topic: Variables
+#Sub-topic: accepting input at run-time and hiding the inpiting character like password
+
+echo "Enter your Username"
+read -s username 
+
+echo "Enter your Password"
+read -s password 
+
+echo "Username: ${username}"
+echo "Password: ${password}"
