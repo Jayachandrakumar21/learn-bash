41142a4e57ca383f8094a01cd5d4289a273bd797
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 17:01:28 +0530

    Bash-script updated

--- functions01.sh
+++ functions01.sh
@@ -3,10 +3,24 @@
 #Topic: Funtions
 #Sub-topic/Scenario:
 
+# Hello(){
+#     echo "Hello!!!! "
+# }
+
+# echo "Before calling function"
+
+# Hello
+
+
 Hello(){
-    echo "Hello!!!! "
+    echo "Hello!!! $1 "
+    echo "Script Name : $0 "
+    echo "Number of args: $# "
+    echo "All args are: $@ "
 }
 
-echo "Before calling function"
+echo "Before calling function, checking name: $1"
+echo "Number of args: $# "
+echo "All args are: $@ 
 
-Hello
+Hello $1
