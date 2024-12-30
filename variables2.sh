1cfc105b468ca7698b22fd9245b1a6f43d08832c
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 11:37:41 +0530

    updated conversation

--- variables2.sh
+++ variables2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Assigning dynamic variables values at run-time
+#Apassing Arguments at run-time
 var1=$1
 var2=$2
 echo "$var1 : How are you $var2"
---
+++ variables3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# created by: Jayachandra_kumar Date:30-12-2024
+# Topic: Variables 
+# Sub-topic: passing variables values at run-time.
+
+echo "Please Enter your Name"
+echo
+read name 
+echo 
+echo "${name}, Welcome to the Devops world!"
