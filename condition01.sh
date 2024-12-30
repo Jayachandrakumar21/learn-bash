6f024b84c76aa8432bd0eae2245c1549ba3f158a
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 16:14:05 +0530

    Bash-script updated

--- condition01.sh
+++ condition01.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 #Created by : Jayachandra_kumar Date:30-12-2024
 #Topic: Conditions
-#Sub-topic: if and else with Exit status: exit-status value will be stored in "$?" by default.
+#Sub-topic: IF and IF else with Exit-status.
 
-lss -ltr
-echo "Exit-status :$?"
+lss -ltr # it will fail and will not excute further
+if [ $? -ne 0 ]; then
+    echo "previous command is failure"
+    exit 1
+fi
+
 ls -ltr
-echo "Exit-status :$?"
+ if [ $? -ne 0 ]; then
+    echo "previous command is failure"
+    exit 1
+fi
+
+echo "program is success"
+
+
 
