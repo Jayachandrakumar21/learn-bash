256024a9f5602e75bf6f049f26603c195d1ed59b
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 16:51:09 +0530

    Bash-script updated

---
+++ functions01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:30-12-2024
+#Topic: Funtions
+#Sub-topic/Scenario:
+
+Hello(){
+    echo "Hello!!!! "
+}
+
+echo "Before calling function"
+
+Hello
