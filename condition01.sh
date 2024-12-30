1c1cd5043d23ce9fb68471ce4c3d45e281ecc1fa
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 15:56:47 +0530

    Bash-script updated

---
+++ condition01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:30-12-2024
+#Topic: Conditions
+#Sub-topic: if and else with Exit status: exit-status value will be stored in "$?" by default.
+
+lss -ltr
+echo "Exit-status :$?"
+ls -ltr
+echo "Exit-status :$?"
+
