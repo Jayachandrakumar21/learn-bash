870b8d3c709654e36796f370263a5ec68688901b
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-30 16:16:02 +0530

    Bash-script updated

--- condition01.sh
+++ condition01.sh
@@ -3,11 +3,11 @@
 #Topic: Conditions
 #Sub-topic: IF and IF else with Exit-status.
 
-lss -ltr # it will fail and will not excute further
-if [ $? -ne 0 ]; then
+#lss -ltr # it will fail and will not excute further
+#if [ $? -ne 0 ]; then
     echo "previous command is failure"
     exit 1
-fi
+#fi
 
 ls -ltr
  if [ $? -ne 0 ]; then
