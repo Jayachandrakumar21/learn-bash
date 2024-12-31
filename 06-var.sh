69d7ea235cc49712ec82877a3bc855ca12cb009c
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2024-12-31 18:44:27 +0530

    script update

---
+++ 06-var.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:30-12-2024
+#Topic: Variables
+#Sub-topic:Special-variables
+
+echo "printind $1 $2 $3 $4"
+echo " All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory : $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$ "
+sleep 60 &
+echo "Process id of last command in background: $!"
