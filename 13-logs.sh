28a092f976a2929c026feefe5a54fea9e8a8f2f7
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2025-01-02 17:44:01 +0530

    bash-scripting

---
+++ 13-logs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:02-01-2025
+#Topic: Bash-script to intall packages 
+#Sub-topic/Scenario: using exit status, functions, conditions and colors
+
+
+
+USERID=$( id -u ) # command to check root user id
+
+# colors
+R="\e[31m" #red
+G="\e[32m" #Green
+Y="\e[33m" #yellow
+N="\e[0m" #Default color
+
+# Redirctores
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE="$(echo $0 | cut -d "." -f1)"
+TIMESTAMP="$(date -%y-%m-%d-%H-%M-%S)"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+# Funtion
+validation(){
+     if [ $1 -ne 0 ]
+    then
+        echo -e "Installing $2 ...$R Failure $N"
+        exit 1
+    else
+        echo -e "Installing $2... $G Success $N"
+    fi
+}
+
+echo "Script started Executing at :$TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error::You should have sudo permission to exicute script $N"
+    exit 1 #other then 0 will exit the script
+fi
+
+
+dnf list installed mysql &>>$LOG_FILE_NAME # commamd to check mysql package is installed or not and redirecting o/p to log file
+if [ $? -ne 0 ] # not installed
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    validation $? "mysql"
+   
+ else
+    echo -e " Mysql already $Y installed.$N"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+if [ $? -ne 0 ] # not installed
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    validation $? "Git"
+ else
+    echo -e " Git already $Y installed. $N"
+fi
