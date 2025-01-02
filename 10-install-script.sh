33059e6bc1fbf31949a728c672ded16de1d6f444
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2025-01-02 16:09:10 +0530

    bash-scripting

---
+++ 10-install-script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:02-01-2025
+#Topic: Bash-script to intall packages 
+#Sub-topic/Scenario: using condition, exit status
+
+USERID=$( id -u )
+if [ $USERID -ne 0 ]
+then
+    echo "Error::You should have sudo permission to exicute script"
+    exit 1 #other then 0 will exit the script
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing mysql... Failure"
+        exit 1
+    else
+        echo "Installing mysql... Success"
+    fi
+else
+    echo " Mysql already installed."
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing git... Failure"
+        exit 1
+    else
+        echo "Installing git... Success"
+    fi
+else
+    echo " git already installed."
+fi
