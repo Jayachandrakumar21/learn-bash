eed731f26633944ac5dc37fb278751d99ee1cabe
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2025-01-02 15:46:40 +0530

    bash-scripting

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,7 +3,7 @@
 #Topic: Conditions
 #Sub-topic/Scenario: finding given number is grater or less then quals to 100
 
-NUMBER=$1
+# NUMBER=$1
 # -gt --> grater then
 # -lt --> less then
 # -eq --> equals to 
@@ -11,7 +11,16 @@ NUMBER=$1
 # -ge --> grater then equals to
 # -le --> less then equals to
 
-if [ $NUMBER -gt 100 ]
+# if [ $NUMBER -gt 100 ]
+# then
+#     echo "Given number is greater then 100"
+# else
+#     echo "Given number is less then or equals to 100"
+# fi
+
+# improvization 
+
+if [ $1 -gt 100 ]
 then
     echo "Given number is greater then 100"
 else
