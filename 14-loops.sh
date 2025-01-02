c12b3edd9d7cb0a4320cbf722dd84c7a6b1a758e
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2025-01-02 20:06:37 +0530

    bash-scripting

---
+++ 14-loops.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Created by : Jayachandra_kumar Date:02-01-2025
+#Topic: loops
+#Sub-topic/Scenario: simple example of loops concept
+# script to print 0 to 5 numbers
+
+for i in {0..5}
+do
+    echo $i
+done
