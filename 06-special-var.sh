61caeb54fdd2448f18e6ec36e2a33941b5e717d4
Jayachandra_Kumar <97940002+Jayachandrakumar21@users.noreply.github.com>
2025-01-17 21:32:34 +0530

    Update 06-special-var.sh

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#Created by : Jayachandra_kumar Date:30-12-2024
+#Created by: Jayachandra_kumar Date:30-12-2024
 #Topic: Variables
-#Sub-topic:Special-variables
+#Sub-topic: Special-variables
 
 echo "printing $1 $2 $3 $4"
 echo " All variables passed: $@"
 echo "Number of variables: $#"
 echo "Script name: $0"
-echo "Present working directory : $PWD"
-echo "Home directory of current user: $HOME"
+echo "Present working directory: $PWD"
+echo "Home directory of the current user: $HOME"
 echo "Which user is running this script: $USER"
-echo "Process id of current script: $$ "
+echo "Process id of the current script: $$ "
 sleep 60 &
-echo "Process id of last command in background: $!"
+echo "Process id of the last command in the background: $!"
