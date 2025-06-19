096dc52079a8e5a1af8335e40090deb5a3de2260
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-06-19 22:32:42 +0530

    bash

--- variables4.sh
+++ variables4.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # execute the command in shell script and thake the out put into variable and printing it.
+# created by : Jayachandra_kumar
 Date=$(date)
 echo "Your shell script started executing at :${Date}"
