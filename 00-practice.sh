31acb7899245c171886b783429dd07ce8fa3ee8d
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-11-28 10:47:36 +0530

    made shome changes in pactice file

--- 00-practice.sh
+++ 00-practice.sh
@@ -2,8 +2,11 @@
 # Topic : Bash practice
 #!/bin/bash
 
-echo " Enter your Name "
-echo
-read name
-echo
-echo " $name, Hi welcome to the DevOps World "
+# echo " Enter your Name "
+# echo
+# read name
+# echo
+# echo " $name, Hi welcome to the DevOps World "
+
+Date=$(date)
+echo " Your shell script started executing at :${Date} "
