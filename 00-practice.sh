90269a76622e404b96b5fc78b25100ddf012cee3
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-11-28 10:59:21 +0530

    made shome changes in pactice file

--- 00-practice.sh
+++ 00-practice.sh
@@ -1,12 +1,25 @@
 # Created by: Jayachandra_kumar
 # Topic : Bash practice
 #!/bin/bash
+# hardcoded variables
+# var1= 'Ramesh'
+# var2= 'Suresh'
+# echo "$var1 : How are your $var2"
+# echo "$var2 : I am fine, thank you $var1"
 
+# # passing Arguments
+# var1= $1
+# var2= $2
+# echo "$var1 : How are your $var2"
+# echo "$var2 : I am fine, thank you $var1"
+
+# passing arguments at run-time
 # echo " Enter your Name "
 # echo
 # read name
 # echo
 # echo " $name, Hi welcome to the DevOps World "
 
-Date=$(date)
-echo " Your shell script started executing at :${Date} "
+# Date=$(date)
+# echo " Your shell script started executing at :${Date} "
+
