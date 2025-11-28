0e400768f30925e1fc179755bd646cd418e53334
jayachandra_kumar <jayachandrakumar21@gmail.com>
2025-11-28 10:19:54 +0530

    new .sh file created to do bash pactice

---
+++ 00-practice.sh
@@ -0,0 +1,10 @@
+# Created by: Jayachandra_kumar
+# Topic : Bash practice
+#!/bin/bash
+
+var1 = Ramesh
+var2 = suresh
+
+echo " $var1 : How are you $var2 "
+echo
+echo " $var2 : I am fine thank you $var1 "
